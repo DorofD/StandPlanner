87bf3b64f3ff1ee00b0eb93169a1f70c73549302
DorofD <legion.0@yandex.ru>
2024-04-08 19:02:41 +0300

    логика резервирования

--- backend/services/reservation_service.py
+++ backend/services/reservation_service.py
@@ -1,5 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor
+from repository.queries.reservations import add_reservation_db
+from errors.intersection_error import IntersectionError
 
 import time
 
@@ -20,7 +22,10 @@ scheduler = run_scheduler()
 
 
 def check_reservation_intersections(stand_id: int, start_time: str, duration: str):
-    pass
+    try:
+        pass
+    except IntersectionError as error:
+        raise error
 
 
 # статусы: planned, active, completed
@@ -28,12 +33,18 @@ def change_reservation_status(id: int, status: str):
     pass
 
 
-def add_reservaiton(user_id: int, stand_id: int, start_time: str, duration: str):
+def create_reservaiton(user_id: int, stand_id: int, start_time: str, duration: str):
     # проверка на пересечение времени с другими резервированиями
     # создание резервирования, получение его id
+    last_row_id = add_reservation_db(
+        user_id=user_id, stand_id=stand_id, start_time=start_time, duration=duration, status='planned')['last_row_id']
+    print(last_row_id)
     # создание start_job и end_job c передачей им id созданного резервирования и получения их id
     # запись полученных id задач в резервирование
     pass
+
+
+create_reservaiton(1, 1, '10:00', '2h')
 
 
 def task_to_run(job_num):
@@ -44,24 +55,24 @@ def task_to_run(job_num):
     print(f'job run{job_num}')
 
 
-current_time += timedelta(minutes=1)
-print(current_time)
-scheduler.add_job(task_to_run, 'date',
-                  run_date=current_time, args=['job 1'], misfire_grace_time=300)
-scheduler.add_job(task_to_run, 'date',
-                  run_date=current_time, args=['job 2'], misfire_grace_time=300)
-scheduler.add_job(task_to_run, 'date',
-                  run_date=current_time, args=['job 3'], misfire_grace_time=300)
-job = scheduler.add_job(task_to_run, 'date',
-                        run_date=current_time, args=['job 4'], misfire_grace_time=300)
-scheduler.add_job(task_to_run, 'date', args=[
-    'job 5'], misfire_grace_time=300)
+# current_time += timedelta(minutes=1)
+# print(current_time)
+# scheduler.add_job(task_to_run, 'date',
+#                   run_date=current_time, args=['job 1'], misfire_grace_time=300)
+# scheduler.add_job(task_to_run, 'date',
+#                   run_date=current_time, args=['job 2'], misfire_grace_time=300)
+# scheduler.add_job(task_to_run, 'date',
+#                   run_date=current_time, args=['job 3'], misfire_grace_time=300)
+# job = scheduler.add_job(task_to_run, 'date',
+#                         run_date=current_time, args=['job 4'], misfire_grace_time=300)
+# scheduler.add_job(task_to_run, 'date', args=[
+#     'job 5'], misfire_grace_time=300)
 
-print('id --------------------------- ', job.id)
+# print('id --------------------------- ', job.id)
 
-print('1-----------')
-print(scheduler.get_jobs())
-print('2-----------')
+# print('1-----------')
+# print(scheduler.get_jobs())
+# print('2-----------')
 
-time.sleep(70)
+# time.sleep(70)
 scheduler.shutdown()
